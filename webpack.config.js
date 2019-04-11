a27606acfa72c0d450262a1b5dc9ed038809273e
oyekunle-mark <oyekunlemac@gmail.com>
2019-04-11 18:10:31 +0100

    Webpack configurations files

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require ('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    sourceMapFilename: 'bundle.map',
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }]
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: 'index.html'
+  })
+};
+
