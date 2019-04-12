a66e20d4cc1c4dbf402763907befc00ad283ad34
oyekunle-mark <oyekunlemac@gmail.com>
2019-04-12 17:49:46 +0100

    Stated the buttons prop and default props

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 const Display = ({ count }) =>
   <div>
     <h1>{count}</h1>
   </div>
+
+Display.propTypes = {
+  count: PropTypes.number.isRequired
+}
+
+Display.defaultProps = {
+  count: 0
+}
 
 export default Display
 
